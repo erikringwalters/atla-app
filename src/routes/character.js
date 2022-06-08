1c676ec06a1e64a49f8512ab114b37e9aac7ae7c
erikringwalters <erik.ring.walters@gmail.com>
2022-06-07 23:23:40 -0700

    card css working

--- src/routes/character.js
+++ src/routes/character.js
@@ -1,20 +1,21 @@
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { getCharacter } from "../character-data";
 
+import Card from "../components/ui/Card";
+
 export default function Character() {
   let navigate = useNavigate();
   let location = useLocation();
   let params = useParams();
   let character = getCharacter(params.characterName);
   return (
-    <main style={{ padding: "1rem" }}>
-      <h2>{character.name}</h2>
-      <img src={character.photo} />
-        
-      <p>{character.description}</p>
-      <p>
-     
-      </p>
-    </main>
+    <div>
+      <Card>
+        <h2>{character.name}</h2>
+        <img src={character.photo} />
+
+        <p>{character.description}</p>
+      </Card>
+    </div>
   );
 }
--- src/routes/characters.js
+++ src/routes/characters.js
@@ -1,3 +1,5 @@
+import Card from "../components/ui/Card";
+
 import {
   useLocation,
   NavLink,
@@ -11,48 +13,47 @@ export default function Characters() {
   let [searchParams, setSearchParams] = useSearchParams();
 
   return (
-    <div style={{ display: "flex" }}>
-      <nav>
-        <input
-          value={searchParams.get("filter") || ""}
-          onChange={(event) => {
-            let filter = event.target.value;
-            if (filter) {
-              setSearchParams({ filter });
-            } else {
-              setSearchParams({});
-            }
-          }}
-        />
-        {characters
-          ?.filter((character) => {
-            let filter = searchParams.get("filter");
-            if (!filter) return true;
-            let name = character.name.toLowerCase();
-            return name.startsWith(filter.toLowerCase());
-          })
-          ?.map((character) => (
-            <NavLink
-              style={({ isActive }) => ({
-                display: "block",
-                margin: "1rem 0",
-                color: isActive ? "red" : "",
-              })}
-              to={`/characters/${character.name}`}
-              key={character.name}
-            >
-              <img src={character.photo} alt={character.name} />
-              {character.name}
-            </NavLink>
-          ))}
-      </nav>
-      <Outlet />
-    </div>
+      <Card>
+        <nav>
+          <input
+            value={searchParams.get("filter") || ""}
+            onChange={(event) => {
+              let filter = event.target.value;
+              if (filter) {
+                setSearchParams({ filter });
+              } else {
+                setSearchParams({});
+              }
+            }}
+          />
+          {characters
+            ?.filter((character) => {
+              let filter = searchParams.get("filter");
+              if (!filter) return true;
+              let name = character.name.toLowerCase();
+              return name.startsWith(filter.toLowerCase());
+            })
+            ?.map((character) => (
+              <NavLink
+                style={({ isActive }) => ({
+                  display: "block",
+                  margin: "1rem 0",
+                  color: isActive ? "red" : "",
+                })}
+                to={`/characters/${character.name}`}
+                key={character.name}
+              >
+                <img src={character.photo} alt={character.name} />
+                {character.name}
+              </NavLink>
+            ))}
+        </nav>
+        <Outlet />
+      </Card>
   );
 
-  function QueryNavLink({to, ...props}) {
-      let location = useLocation();
-      return <NavLink to={to + location.search} {...props} />;
+  function QueryNavLink({ to, ...props }) {
+    let location = useLocation();
+    return <NavLink to={to + location.search} {...props} />;
   }
-
 }
