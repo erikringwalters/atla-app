837aa02fb05a022448c00493bce64e302339dceb
erikringwalters <erik.ring.walters@gmail.com>
2022-06-27 19:05:21 -0700

    root redirects to home

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './fonts/Herculanum/Herculanum-Regular.woff';
 
 import Home from "./components/home/home";
@@ -17,7 +17,8 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="/home" to="/characters" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Navigate replace to="/home" />} />
         <Route path="/creatures" element={<Creatures />} />
         <Route path="/characters" element={<Characters />}>
         </Route>
