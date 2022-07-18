bde4936f581b4c86d6e6b7b5f27c47f6d7734d54
erikringwalters <erik.ring.walters@gmail.com>
2022-07-17 17:27:44 -0700

    perspective card moving on its own

--- src/components/movingCard/movingCard.js
+++ src/components/movingCard/movingCard.js
@@ -34,24 +34,25 @@ export default function MovingCard() {
 
   return (
 
-  <div id="card" className="perspective-card">
-    <div className="perspective-card__transformer">
+    <div className="cardContainer">
+  <div id="card" className="perspective-card" data-decorator="PerspectiveCard" data-ambient>
+    <div className="perspective-card__transformer" >
       <div className="perspective-card__artwork.perspective-card__artwork--front
       img">
         <img
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/982762/9b1b5b5-1.png"
         />
       </div>
-      <div className="perspective-card__artwork.perspective-card__artwork--back
+      {/* <div className="perspective-card__artwork.perspective-card__artwork--back
       img">
         <img
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/982762/pokemon_card_backside_in_high_resolution_by_atomicmonkeytcg_dah43cy-fullview.png"
         />
-      </div>
+      </div> */}
       <div className="perspective-card__shine"></div>
     </div>
   </div>
-
+  </div>
 
     // <div id="card" className="perspective-card" data-decorator="PerspectiveCard">
     //   <img
