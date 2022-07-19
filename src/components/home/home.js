5fff9b19c4274a032c346ff0ffa17b3b65b0cc47
erikringwalters <erik.ring.walters@gmail.com>
2022-07-18 22:13:26 -0700

    aang card moving (very rough)

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,22 +4,49 @@ import Card from "../ui/Card";
 import { getCharacter } from "../../character-data";
 import { getCreature } from "../../creature-data";
 import MovingCard from "../movingCard/movingCard";
+import PerspectiveCard from "wtc-perspective-card";
+import { useEffect } from "react";
+// import "./movingCard.css";
 
 export default function Home() {
   let character = getCharacter("Aang");
   let creature = getCreature("Flying Bison");
-  const card = new MovingCard(document.getElementById("card"));
+
+  useEffect(() => {
+    if (document.getElementById("card")) {
+      const card = new PerspectiveCard(document.getElementById("card"));
+      // console.log(card);
+    }
+  }, []);
 
   return (
     <div id="homeDiv">
-      <div>
-        <MovingCard
-          id="card"
-          name={character.name}
-          element={character.element}
-          photo={character.photo}
-          description={character.description}
-        ></MovingCard>
+      <div >
+        <div id="card" className="perspective-card" data-decorator="PerspectiveCard" data-ambient>
+          <div className="perspective-card__transformer">
+            <div className="card__artwork perspective-card__artwork--front">
+              <div className="cardContainer" id="card">
+                <Card className="card">
+                  <div className={"cardContent " + character.element}>
+                    <h3 className="cardTitle">{character.name}</h3>
+                    <div
+                      className={
+                        "perspective-card__artwork card__artwork--front cardImgContainer " + character.element + "Img"
+                      }
+                    >
+                      <img
+                        className={"cardImg"}
+                        src={character.photo}
+                        alt={character.name}
+                      />
+                    </div>
+                    <div className="perspective-card__shine"></div>
+                  </div>
+                </Card>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <Link to="/characters">
           <h2>Characters</h2>
@@ -40,8 +67,8 @@ export default function Home() {
             </Card>
           </div>
         </Link>
+        {/*
       </div>
-
       <div>
         <Link to="/creatures">
           <h2>Creatures</h2>
@@ -59,7 +86,7 @@ export default function Home() {
               </div>
             </Card>
           </div>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
--- src/components/movingCard/movingCard.js
+++ src/components/movingCard/movingCard.js
@@ -3,8 +3,13 @@ import "./movingCard.css";
 import "./character.css";
 import Card from "../ui/Card";
 import { useEffect } from "react";
-import { useParams, useNavigate, useLocation, NavLink, Link } from "react-router-dom";
-
+import {
+  useParams,
+  useNavigate,
+  useLocation,
+  NavLink,
+  Link,
+} from "react-router-dom";
 
 export default function MovingCard(props) {
   useEffect(() => {
@@ -14,42 +19,35 @@ export default function MovingCard(props) {
   }, []);
 
   return (
-    <div className="cardContainer">
-      <div
-        id="card"
-        className="perspective-card"
-        data-decorator="PerspectiveCard"
-        data-ambient
-      >
-        <div className="perspective-card__transformer">
-          <div
-            className="perspective-card__artwork.perspective-card__artwork--front img"
-          >
-            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/982762/9b1b5b5-1.png" />
-          </div>
-          <div className="perspective-card__shine"></div>
-        </div>
-      </div>
-    </div>
-
-    // <div>
-    //   <div className="cardWDetails">
-    //     <Card className="card">
-    //       <div className={"cardContent " + props.element}>
-    //         <h3 className="cardTitle">{props.name}</h3>
-    //         <div className={"cardImgContainer " + props.element + "Img"}>
-    //           <img
-    //             className={"cardImg"}
-    //             src={props.photo}
-    //             alt={props.name}
-    //           />
-    //         </div>
+    // <div className="cardContainer">
+    //   <div
+    //     id="card"
+    //     className="perspective-card"
+    //     data-decorator="PerspectiveCard"
+    //     data-ambient
+    //   >
+    //     <div className="perspective-card__transformer">
+    //       <div
+    //         className="perspective-card__artwork.perspective-card__artwork--front img"
+    //       >
+    //         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/982762/9b1b5b5-1.png" />
     //       </div>
-    //     </Card>
-    //     <div className="desc">
-    //       <p>{props.description}</p>
+    //       <div className="perspective-card__shine"></div>
     //     </div>
     //   </div>
     // </div>
+
+    <div className="cardContainer">
+      {/* // <Card>
+      //   <div className={props.element}>
+      //     <h3 className="cardTitle">{props.name}</h3>
+      //     <div className={"cardImgContainer " + props.element + "Img"}>
+      //       <div>
+      //         <img className={"cardImg"} src={props.photo} alt={props.name} />
+      //       </div>
+      //     </div>
+      //   </div>
+      // </Card> */}
+    </div>
   );
 }
